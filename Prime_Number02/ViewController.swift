eb125ce308e778b706d367e6a835f78a3ebc4737
Jonghyun Kim <jhkim@dit.ac.kr>
2019-04-09 11:06:31 +0900

    2nd commit

--- Prime_Number02/ViewController.swift
+++ Prime_Number02/ViewController.swift
@@ -1,25 +1,65 @@
-//
-//  ViewController.swift
-//  Prime_Number02
-//
-//  Created by D7702_09 on 2018. 3. 28..
-//  Copyright © 2018년 lse. All rights reserved.
-//
 
+// Prime Number Checker
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet weak var LblA: UILabel!
+    @IBOutlet weak var textA: UITextField!
+    
+    var isPrime = true
+    
+    @IBAction func Check(_ sender: Any) {
+        if let number = Int(textA.text!) {
+            print(number)
+            var isPrime = true
+            
+            if number <= 0 {
+                LblA.text = "input correct value!"
+                textA.text = ""
+                return
+            }
+            
+            if number == 1 {
+                isPrime = false
+            }
+        
+            if number != 1 && number != 2 {
+                for i in 2 ..< number {
+                    if number % i == 0 {
+                        isPrime = false
+                    }
+                }
+            }
+        
+            if isPrime == true {
+                LblA.text = "prime number"
+            } else {
+                LblA.text = "Not Prime number"
+            }
+            textA.text = ""
+        } else {
+            print("nil 발생")
+            LblA.text = "input numeric value!"
+            textA.text = ""
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textA.delegate = self
+        textA.placeholder = "ex) 7"
+        textA.clearButtonMode = UITextField.ViewMode.always
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
-
-
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textA.resignFirstResponder()
+        return true
+    }
 }
+
 
